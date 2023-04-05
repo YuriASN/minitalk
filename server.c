0d0fff143f636a88692da456ad4442e5a5dcf972
YuriASN <yuriasn@hotmail.com>
2023-04-05 20:11:45 +0100

    Server started working. Now have to clear debbugs and run tests.

---
+++ server.c
@@ -0,0 +1,99 @@
+#include "minitalk.h"
+
+/* Transforms the first 32 bits on a int that contains the string size.
+When done, it restore index and value after atrributing it to leng.
+Returns a string allocated with size of leng. */
+static char	*get_leng(int sig, int *leng)
+{
+	static int	i;
+	static int	value;
+	char		*str;
+
+	if (sig == SIGUSR2)
+		value |= 1;
+	if (i == 31)
+	{
+//write(1, "leng = ", 7); ft_putnbr_fd(value, 2); write(1, "\n", 1);
+		i = 0;
+		str = ft_calloc(sizeof(char *), value + 1);
+		if (!str)
+		{
+			ft_printf("Malloc for string failed.\n");
+			exit (1);
+		}
+		*leng = value;
+		value = 0;
+		return (str);
+	}
+	value <<= 1;
+	++i;
+	return (0);
+}
+
+/* Transforms the 8 bites into a char.
+Returns TRUE if char is complete in order to move to the next. */
+static int	get_str(int sig, char *str)
+{
+	static int	i;
+	static unsigned char	c;
+
+	/* if (sig == SIGUSR1)
+		write(2, "0", 1);
+	else
+		write(2, "1", 1); */
+	if (sig == SIGUSR2)
+		c |= 1;
+//ft_putnbr_fd(i, 2);
+	if (i == 7)
+	{
+		str[0] = (char)c;
+//ft_putnbr_fd((int)c, 1);
+		i = 0;
+//write(2, "char pronto\t", 12); write(2, &c, 1); write(2, "\n", 1);
+		c = 0;
+		return (1);
+	}
+	c <<= 1;
+	++i;
+	return (0);
+}
+
+static void	signal_handler(int sig)
+{
+	static char	*str;
+	static int	leng;
+	static int	i;
+
+	if (!leng){
+		str = get_leng(sig, &leng);}
+	else if (i < leng)
+	{
+		if (get_str(sig, &str[i]))
+			++i;
+	}
+	if (leng && i == leng)
+	{
+		leng = 0;
+		i = 0;
+		ft_putstr_fd(str, 1);
+		write(1, "\n", 1);
+		free(str);
+		str = 0;
+	}
+}
+
+int	main(void)
+{
+	int					pid;
+	struct sigaction	sas;
+
+	sas.sa_handler = &signal_handler;
+	pid = getpid();
+	ft_putnbr_fd(pid, 1);
+	write(1, "\n", 1);
+	sigaction(SIGUSR1, &sas, NULL);
+	sigaction(SIGUSR2, &sas, NULL);
+	while (1)
+		usleep(100);
+	exit (0);
+}
