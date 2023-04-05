93d23ff10a1eeb9fd5052878f07d40c871e2bae9
YuriASN <yuriasn@hotmail.com>
2023-04-05 02:57:19 +0100

    client allegedly done.

---
+++ client.c
@@ -0,0 +1,105 @@
+#include "minitalk.h"
+
+/* Return TRUE if PID is all digits or FALSE if anything else is found. */
+static int	check_pid(char *str, int *pid)
+{
+	int	i;
+
+	i = -1;
+	while (str[++i])
+	{
+		if (!ft_isdigit((int)str[i]))
+		{
+			ft_printf("PID must be only numbers.\n");
+			return (0);
+		}
+	}
+	*pid = ft_atoi(str);
+	if (*pid < 2)
+	{
+		ft_printf("PID isn't a valid process.\n");
+		return (0);
+	}
+	return (1);
+}
+
+/* Sends a int via SIGUSR1 (bit 0) and SIGUSR2 (bit 1). */
+static int	send_int(int pid, int str_len)
+{
+	int	i;
+
+	i = 32;
+	while (--i >= 0)
+	{
+		if (!(str_len >> i) & 1)
+		{
+			if ((kill(pid, SIGUSR1)) == -1)
+			{
+				ft_printf("Kill function returned an error.\n");
+				return (0);
+			}
+		}
+		else
+		{
+			if ((kill(pid, SIGUSR2)) == -1)
+			{
+				ft_printf("Kill function returned an error.\n");
+				return (0);
+			}
+		}
+		usleep(100);
+	}
+	return (1);
+}
+
+/* Sends a string via SIGUSR1 (bit 0) and SIGUSR2 (bit 1). */
+static int	send_str(int pid, char *str)
+{
+	int	i;
+	int	j;
+
+	j = -1;
+	while (str[++j])
+	{
+		i = 8;
+		while (--i >= 0)
+		{
+			if (!(str[j] >> i) & 1)
+			{
+				if ((kill(pid, SIGUSR1)) == -1)
+					return (0);
+			}
+			else
+			{
+				if ((kill(pid, SIGUSR2)) == -1)
+					return (0);
+			}
+			usleep(100);
+		}
+	}
+	return (1);
+}
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+	int	str_len;
+
+	if (argc != 3)
+	{
+		ft_printf("Invalid number of arguments.\n");
+		exit (1);
+	}
+	if (!check_pid(argv[1], &pid))
+		exit (1);
+	str_len = ft_strlen(argv[2]);
+	if (!send_int(pid, str_len))
+		exit (1);
+	if (!send_str(pid, argv[2]))
+	{
+		ft_printf("Kill function returned an error.\n");
+		exit (1);
+	}
+	send_str(pid, "\0");
+	exit (0);
+}
---
+++ minitalk.h
@@ -0,0 +1,17 @@
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# define WHT "\e[0;97m"
+# define CYN "\e[0;96m"
+# define MAG "\e[0;95m"
+# define BLU "\e[0;94m"
+# define YEL "\e[0;93m"
+# define GRN "\e[0;92m"
+# define RED "\e[0;91m"
+# define BLK "\e[0;90m"
+# define CRESET "\e[0m"
+
+# include "libft/libft.h"
+# include <signal.h>
+
+#endif
