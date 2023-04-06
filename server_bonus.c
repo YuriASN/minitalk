0d0558bb1ddcd7eae608ccd30b76d10f2777ede4
YuriASN <yuriasn@hotmail.com>
2023-04-07 00:52:49 +0100

    Fixed norminette issues.

--- server_bonus.c
+++ server_bonus.c
@@ -6,21 +6,14 @@ static void	get_nbr(int sig, int *nbr)
 {
 	static int	i;
 	static int	value;
-	//write(2, "*nbr = ", 7);	ft_putnbr_fd(*nbr, 2);	write(2, "\n", 1);
-	if (sig == SIGUSR2)		{//	write(2, "1", 1);
-		value |= 1;}
-//	else
-//			write(2, "0", 1);
+
+	if (sig == SIGUSR2)
+		value |= 1;
 	if (i == 31)
 	{
-//		write(2, "\n", 1);
-//		write(2, "*b4 nbr = ", 9);	ft_putnbr_fd(*nbr, 2);	write(2, "\n", 1);
 		*nbr = value;
 		i = 0;
 		value = 0;
-//		write(2, "*nbr = ", 6);	ft_putnbr_fd(*nbr, 2);	write(2, "\n", 1);
-//		write(2, "value = ", 8);	ft_putnbr_fd(value, 2);	write(2, "\n", 1);
-//		write(2, "i = ", 4);	ft_putnbr_fd(i, 2);	write(2, "\n\n", 2);
 		return ;
 	}
 	value <<= 1;
@@ -34,13 +27,10 @@ static int	get_str(int sig, char *str)
 	static int				i;
 	static unsigned char	c;
 
-	if (sig == SIGUSR2)		{//write(2, "1", 1);
-		c |= 1;}
-//	else
-//		write(2, "0", 1);
+	if (sig == SIGUSR2)
+		c |= 1;
 	if (i == 7)
 	{
-//		write(2, "\n", 1);
 		str[0] = (char)c;
 		i = 0;
 		c = 0;
@@ -51,6 +41,25 @@ static int	get_str(int sig, char *str)
 	return (0);
 }
 
+/* Get data for leng and pid_c. After done it returns the str alloced. */
+static char	*get_data(int *leng, int *pid_c, int sig)
+{
+	char	*str;
+
+	if (!*leng)
+		get_nbr(sig, leng);
+	else if (!*pid_c)
+		get_nbr(sig, pid_c);
+	if (*pid_c)
+	{
+		str = ft_calloc(sizeof(char *), *leng + 1);
+		if (!str)
+			exit (1);
+		return (str);
+	}
+	return (0);
+}
+
 static void	signal_handler(int sig)
 {
 	static char	*str;
@@ -58,17 +67,8 @@ static void	signal_handler(int sig)
 	static int	i;
 	static int	pid_c;
 
-	if (!leng)
-		get_nbr(sig, &leng);
-	else if (!pid_c)
-		get_nbr(sig, &pid_c);
-	if (pid_c && !str)
-	{
-//		write(2, "Malloc using leng = ", 20);	ft_putnbr_fd(leng, 2);	write(2, "\n", 1);
-		str = ft_calloc(sizeof(char *), leng + 1);
-		if (!str)
-			exit (1);
-	}
+	if (!leng || !pid_c)
+		str = get_data(&leng, &pid_c, sig);
 	else if (pid_c && i < leng)
 	{
 		if (get_str(sig, &str[i]))
@@ -78,10 +78,8 @@ static void	signal_handler(int sig)
 	{
 		usleep(100);
 		if (kill(pid_c, SIGUSR1) == -1)
-			write(1, "Failed to send signal back.", 27);
-//		write(2, "pid_c = ", 8);	ft_putnbr_fd(pid_c, 2);	write(2, "\n", 1);
-//		write(2, "Malloc = ", 9);	ft_putnbr_fd(leng, 2);	write(2, "\n", 1);
-		ft_putstr_fd(str, 1);
+			write(1, "Failed to send signal back.\n", 28);
+		write(1, str, leng);
 		write(1, "\n", 2);
 		free(str);
 		str = 0;
